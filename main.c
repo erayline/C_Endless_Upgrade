006f7c6fe7a71d72632c609587df8f3145dfbfd1
erayline <144234072+erayline@users.noreply.github.com>
2023-11-27 23:02:11 +0300

    Update main.c
    i fixed shooting weirdness. fixed moving weirdness, added multiple enemy. and added color to enemys.

--- main.c
+++ main.c
@@ -3,42 +3,36 @@
 
 #define FPS = 30
 #define FRAME_TARGET_TIME = (1000/FPS) //her frame kaç ms sürecek
+#define enemy_count 10
 int last_frame_time = 0;
 
 typedef struct {
 	int x, y;
 	int life;
-	char* name;
 	int size;
+	int puan;
 } Man;
-
 typedef struct {
-	int x;
-	int y;
+	float x;
+	float y;
 	int life;
 	int width;
 	int height;
 }Bullet;
-
 typedef struct {
-	int x;
-	int y;
+	float x;
+	float y;
 	int color;
 	int life;
 	int width;
 	int height;
-
+	int speed;
 }Enemy;
-
- 
-
-int processEvents(SDL_Window *window, Man *man,Bullet *mermi, Enemy *enemy) {
+int processEvents(SDL_Window *window, Man *man,Bullet *mermi, Enemy *enemy, Enemy *enemyler) {
 	SDL_Event event;
 	int done = 0;
 	
-
 	float delta_time = (SDL_GetTicks() - last_frame_time) / 1000.0f;
-
 	last_frame_time = SDL_GetTicks();
 
 	
@@ -61,14 +55,12 @@ int processEvents(SDL_Window *window, Man *man,Bullet *mermi, Enemy *enemy) {
 					done = 1;
 					break;
 				case SDLK_SPACE:
-					mermi->x = man->x + (man->size)/2 - (mermi->width)/2;
-					mermi->y = man-> y;
-					mermi->life = 1;
-					break;
-				case SDLK_0:
-					enemy->life += 1;
-					enemy->x = 0;
-					enemy->y = 0;
+					if (mermi->life != 1) {
+						mermi->x = man->x + (man->size)/2 - (mermi->width)/2;
+						mermi->y = man-> y;
+						mermi->life = 1;
+					}
+
 					break;
 			}
 			break;
@@ -92,8 +84,9 @@ int processEvents(SDL_Window *window, Man *man,Bullet *mermi, Enemy *enemy) {
 	if (state[SDL_SCANCODE_DOWN]){
 		man->y += 400 * delta_time;
 	}
+
 	if (mermi->life == 1) {
-		mermi->y -= 7;
+		mermi->y -= 1000 * delta_time;
 	}
 	if (mermi->y < 0) {
 		mermi->life = 0;
@@ -103,45 +96,50 @@ int processEvents(SDL_Window *window, Man *man,Bullet *mermi, Enemy *enemy) {
 		mermi->y = -2000;
 		mermi->x = -2000;
 	}
-	if (enemy->life <= 0) {
-		enemy->y = -1000;
-		enemy->x = -1000;
+
+	for (int n = 0; n < enemy_count; n++) {
+		
+		if (enemyler[n].life <= 0) {
+			enemyler[n].y = -1000;
+			enemyler[n].x = -1000;
+		}
+		else {
+			if (enemyler[n].life != 0) {
+				if (enemyler[n].y + enemyler[n].height/2 > man->y + man->size/2) {
+					enemyler[n].y -= enemyler[n].speed * delta_time;
+				}
+				if (enemyler[n].y + enemyler[n].height / 2 < man->y + man->size/2) {
+					enemyler[n].y += enemyler[n].speed * delta_time;
+				}
+
+				if (enemyler[n].x + enemyler[n].width / 2 > man->x + man->size / 2) {
+					enemyler[n].x -= enemyler[n].speed * delta_time;
+				}
+				if (enemyler[n].x + enemyler[n].width / 2 < man->x + man->size / 2) {
+					enemyler[n].x += enemyler[n].speed * delta_time;
+				}
+			}
+
+			if ((((enemyler[n].x < mermi->x) && (enemyler[n].x + enemyler[n].width > mermi->x)) || ((enemyler[n].x < mermi->x + mermi->width) && (enemyler[n].x + enemyler[n].width > mermi->x + mermi->width))) && (((enemyler[n].y < mermi->y) && (enemyler[n].y + enemyler[n].height > mermi->y)) || ((enemyler[n].y < mermi->y + mermi->height) && (enemyler[n].y + enemyler[n].height > mermi->y + mermi->height)))) {
+				enemyler[n].life -= 1;
+				man->puan += 1;
+				mermi->life = 0;
+			}
+
+		}
+
+
 	}
 
-	 
-	if (enemy->life != 0) {
-		if (enemy->y + enemy->height/2 > man->y + man->size/2) {
-			enemy->y -= 150 * delta_time;
-		}
-		if (enemy->y + enemy->height / 2 <= man->y + man->size/2) {
-			enemy->y += 150 * delta_time;
-		}
-		if (enemy->x + enemy->width / 2 > man->x + man->size / 2) {
-			enemy->x -= 150 * delta_time;
-		}
-		if (enemy->x + enemy->width / 2 <= man->x + man->size / 2) {
-			enemy->x += 150 * delta_time;
-		}
-	}
 
 
-
-	if ((((enemy->x < mermi->x) && (enemy->x + enemy->width > mermi->x)) || ((enemy->x < mermi->x + mermi->width) && (enemy->x + enemy->width > mermi->x + mermi->width))) && (((enemy->y < mermi->y) && (enemy->y + enemy->height > mermi->y)) || ((enemy->y < mermi->y + mermi->height) && (enemy->y + enemy->height > mermi->y + mermi->height)))) {
-		enemy->life -= 1;
-		mermi->life = 0;
-	}
-
-	int x_fare, y_fare;
-	SDL_GetMouseState(&x_fare, &y_fare);
-	printf("%d ", x_fare);
-	printf("%d\n", y_fare);
 
 	return done;
 }
 
 
 
-void doRender(SDL_Renderer* renderer, Man* man, Bullet *mermi, Enemy *enemy) {
+void doRender(SDL_Renderer* renderer, Man* man, Bullet *mermi, Enemy *enemy, Enemy *enemyler) {
 
 	SDL_SetRenderDrawColor(renderer, 50, 10, 30, 255);
 	SDL_RenderClear(renderer);
@@ -157,11 +155,30 @@ void doRender(SDL_Renderer* renderer, Man* man, Bullet *mermi, Enemy *enemy) {
 
 	}
 
-	if (enemy->life != 0) {
-		SDL_Rect enemy_1 = { enemy->x, enemy->y,enemy->width,enemy->height };
-		SDL_SetRenderDrawColor(renderer, 204, 204, 20, 255);
-		SDL_RenderFillRect(renderer, &enemy_1);
+	SDL_Rect enemy_1[enemy_count];
+	SDL_Rect enemy_1_dec[enemy_count];
+	for (int n = 0; n < enemy_count; n++) {
+		enemy_1[n].x = enemyler[n].x;
+		enemy_1[n].y = enemyler[n].y;
+		enemy_1[n].w = enemyler[n].width;
+		enemy_1[n].h = enemyler[n].height;
+
+		enemy_1_dec[n].x = enemyler[n].x + enemyler[n].width/4;
+		enemy_1_dec[n].y = enemyler[n].y + enemyler[n].height / 4;
+		enemy_1_dec[n].w = enemyler[n].width - enemyler[n].width/2;
+		enemy_1_dec[n].h = enemyler[n].height - enemyler[n].height / 2;
 	}
+
+	for (int n = 0; n < enemy_count; n++) {
+
+		if (enemyler[n].life != 0) {
+			SDL_SetRenderDrawColor(renderer, 204, 204, 20, 255);
+			SDL_RenderFillRect(renderer, &enemy_1[n]);
+			SDL_SetRenderDrawColor(renderer, 100, 20, 190, 255);
+			SDL_RenderFillRect(renderer, &enemy_1_dec[n]);
+		}
+	}
+
 
 	SDL_RenderPresent(renderer);
 }
@@ -173,10 +190,13 @@ int main(int argc, char* argv[]) {
 	SDL_Renderer* renderer; //renderer tanımlama
 	SDL_Init(SDL_INIT_EVERYTHING); // sdl'i çalıştırıyor 
 
+
+
 	Man man;
 	man.x = 220;
 	man.y = 110;
 	man.size = 50;
+	man.puan = 0;
 	
 	Bullet mermi;
 	mermi.x = -10; 
@@ -190,13 +210,22 @@ int main(int argc, char* argv[]) {
 	enemy.width = 60;
 	enemy.height = 60;
 	enemy.life = 1;
-	
+
+	Enemy enemyler[enemy_count];
+	for (int n = 0; n < enemy_count; n++) {
+		enemyler[n].x = n * 100;
+		enemyler[n].y = 400;
+		enemyler[n].width = 40 + n*4;
+		enemyler[n].height =  40 + n*4;
+		enemyler[n].life = 1;
+		enemyler[n].speed =  140 - n * 10 ;
+	}
 
 	
 
 
 	window = SDL_CreateWindow("Game",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,1024,740,SDL_WINDOW_BORDERLESS);
-	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
 
 
 
@@ -204,9 +233,9 @@ int main(int argc, char* argv[]) {
 	int done = 0;
 	while (!done) {
 
-		done = processEvents(window, &man, &mermi, &enemy);
+		done = processEvents(window, &man, &mermi, &enemy, &enemyler);
 		//render display
-		doRender(renderer, &man, &mermi, &enemy);
+		doRender(renderer, &man, &mermi, &enemy,&enemyler);
 	}
 
 	
