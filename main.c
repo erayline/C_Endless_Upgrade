fb91bc8029aeb3d987a363607da9e5740ab5cf39
erayline <144234072+erayline@users.noreply.github.com>
2023-11-29 22:16:35 +0300

    Update main.c

--- main.c
+++ main.c
@@ -9,11 +9,13 @@
 #include <math.h>
 
 
-#define FPS = 30
+#define FPS = 200
 
 #define FRAME_TARGET_TIME = (1000/FPS) //her frame kaç ms sürecek
-#define enemy_count 6
+#define enemy_count 7
 
+float speed_constant;
+int last_bullet_shot=10;
 void updateText(SDL_Renderer* renderer, TTF_Font* font, SDL_Surface** textSurface, SDL_Texture** textTexture, const char* newText) {
 	// Free existing text resources if necessary
 	if (*textSurface) {
@@ -54,12 +56,11 @@ int ilk_buyukmu(int deger1, int deger2) {
 }
 
 int atack_speed = 400;
-int bullet_speed = 300;
+int bullet_speed_interval = 300;
 int last_frame_time = 1;
-int biterken = 0;
-int bullet_x_speed = 0;
-int bullet_y_speed = 0; // burada birsürü gereksiz şey tanımlamışım bir ara azaltıcam.
-int processEvents(SDL_Window* window, Man* man, Bullet* mermi, Enemy* enemyler, Mix_Chunk* hitEffect, Mix_Chunk* shootEffect) { // burada işlemler oluyor
+float bullet_x_speed = 0;
+float bullet_y_speed = 0; // burada birsürü gereksiz şey tanımlamışım bir ara azaltıcam.
+int processEvents(SDL_Window* window, Man* man, Bullet* mermiler, Enemy* enemyler, Mix_Chunk* hitEffect, Mix_Chunk* shootEffect) { // burada işlemler oluyor
 
 	int man_ortasiX = man->x + man->size / 2;
 	int man_ortasiY = man->y + man->size / 2;  // sürekli adamın ortasını bulmaya çalıştığım için kısalttım bu şekilde
@@ -130,7 +131,7 @@ int processEvents(SDL_Window* window, Man* man, Bullet* mermi, Enemy* enemyler, 
 				done = 1;
 				break;
 			case SDLK_SPACE: //denemek için
-				mermi->life = 0;
+				bullet_speed_interval -= 10;
 				break;
 			}
 			break;
@@ -150,51 +151,53 @@ int processEvents(SDL_Window* window, Man* man, Bullet* mermi, Enemy* enemyler, 
 	for (int n = 0; n < enemy_count; n++) { //burada range içine giren eleman olursa ateş etsin diye ayarladım
 
 		if (((((enemyler[n].x > man->x - man->range) && (enemyler[n].x < man->x + man->range)) && ((enemyler[n].y > man->y - man->range) && (enemyler[n].y < man->y + man->range))) || (((enemyler[n].x + enemyler[n].width > man->x - man->range) && (enemyler[n].x + enemyler[n].width < man->x + man->range)) && ((enemyler[n].y + enemyler[n].height > man->y - man->range) && (enemyler[n].y + enemyler[n].height < man->y + man->range))))) {
-
-			if (mermi->life != 1) { // halihazırda ateşlenmemiş mermiyi ateşliyor
-				if (atack_speed < (SDL_GetTicks() - biterken)) { //burada iki saniyede bir ateş etsin diye ayarlamaya çalıştım 
-					mermi->x = man->x + (man->size) / 2 - (mermi->width) / 2;
-					mermi->y = man->y + man->size / 2 - mermi->width/2;
-					mermi->life = 1;
-					Mix_PlayChannel(-1, shootEffect, 0);
-				}
-				else {
-					//printf("mermi zaamanı gelmedi");
+		
+			for (int i = 0; i < 30; i++) {
+				if (mermiler[i].life != 1) { // halihazırda ateşlenmemiş mermiyi ateşliyor
+					if (bullet_speed_interval < (SDL_GetTicks() - last_bullet_shot)) { //burada iki saniyede bir ateş etsin diye ayarlamaya çalıştım 
+						if ( 1 < (SDL_GetTicks() - mermiler[i].biterken)) {
+							mermiler[i].x = man->x + (man->size) / 2 - (mermiler[i].width) / 2;
+							mermiler[i].y = man->y + man->size / 2 - mermiler[i].width / 2;
+							mermiler[i].life = 1;
+							Mix_PlayChannel(-1, shootEffect, 0);
+							last_bullet_shot = SDL_GetTicks();
+						}
+					}
 				}
 			}
+		}
+	}
+	for (int i = 0; i < 30; i++) {
 
+		if (mermiler[i].life == 1) {
+			mermiler[i].x -= mermiler[i].speedx;
+			mermiler[i].y -= mermiler[i].speedy;
 		}
 
 
+		if (mermiler[i].y < 0 || mermiler[i].x < 0 || mermiler[i].y > WINDOW_HEIGHT || mermiler[i].x >WINDOW_WIDTH) {
+			mermiler[i].life = 0;
+			mermiler[i].biterken = SDL_GetTicks();
+		}
 	}
 
 
-	if (mermi->life == 1) {
-		mermi->x -= mermi->speedx;
-		mermi->y -= mermi->speedy;
-	}
-
-
-
-	if (mermi->y < 0) {
-		mermi->life = 0;
-		biterken = SDL_GetTicks();
-	}
-	float speed_constant = 300;
+	speed_constant = 300;
 	float diagnal = uzaklikSirasi[enemy_count - 1][0];
 
 	float cosforx = (man_ortasiX - enemyler[closest_enemy_index].x - enemyler[closest_enemy_index].width / 2) / diagnal;
 	float sinfory = (man_ortasiY - enemyler[closest_enemy_index].y - enemyler[closest_enemy_index].height / 2) / diagnal;
-	if (mermi->life <= 0) {
+	
+	for (int i = 0; i < 30; i++) {
 
-		mermi->speedx =cosforx * speed_constant * delta_time;
-		mermi->speedy = sinfory * speed_constant * delta_time;
-		
-		mermi->y = 2000;
-		mermi->x = 2000;
+		if (mermiler[i].life <= 0) {
+			mermiler[i].speedx = cosforx * speed_constant * delta_time;
+			mermiler[i].speedy = sinfory * speed_constant * delta_time;
 
+			mermiler[i].y = 2000;
+			mermiler[i].x = 2000;
+		}
 	}
-
 
 
 	if (state[SDL_SCANCODE_RIGHT]) {
@@ -234,15 +237,15 @@ int processEvents(SDL_Window* window, Man* man, Bullet* mermi, Enemy* enemyler, 
 			}
 
 
-
-			if ((((enemyler[n].x < mermi->x) && (enemyler[n].x + enemyler[n].width > mermi->x)) || ((enemyler[n].x < mermi->x + mermi->width) && (enemyler[n].x + enemyler[n].width > mermi->x + mermi->width))) && (((enemyler[n].y < mermi->y) && (enemyler[n].y + enemyler[n].height > mermi->y)) || ((enemyler[n].y < mermi->y + mermi->height) && (enemyler[n].y + enemyler[n].height > mermi->y + mermi->height)))) {
-				enemyler[n].life -= 1;
-				man->puan += 1; // mermi için collision detection
-				mermi->life = 0;
-				Mix_PlayChannel(-1, hitEffect, 0);
-				biterken = SDL_GetTicks();
+			for (int i = 0; i < 30; i++) {
+				if ((((enemyler[n].x < mermiler[i].x) && (enemyler[n].x + enemyler[n].width > mermiler[i].x)) || ((enemyler[n].x < mermiler[i].x + mermiler[i].width) && (enemyler[n].x + enemyler[n].width > mermiler[i].x + mermiler[i].width))) && (((enemyler[n].y < mermiler[i].y) && (enemyler[n].y + enemyler[n].height > mermiler[i].y)) || ((enemyler[n].y < mermiler[i].y + mermiler[i].height) && (enemyler[n].y + enemyler[n].height > mermiler[i].y + mermiler[i].height)))) {
+					enemyler[n].life -= 1;
+					man->puan += 1; // mermi için collision detection
+					mermiler[i].life = 0;
+					Mix_PlayChannel(-1, hitEffect, 0);
+					mermiler[i].biterken = SDL_GetTicks();
+				}
 			}
-
 		}
 
 
@@ -257,7 +260,7 @@ int processEvents(SDL_Window* window, Man* man, Bullet* mermi, Enemy* enemyler, 
 
 
 
-void doRender(SDL_Renderer* renderer, Man* man, Bullet* mermi, Enemy* enemyler) {
+void doRender(SDL_Renderer* renderer, Man* man, Bullet* mermiler, Enemy* enemyler) {
 
 	SDL_SetRenderDrawColor(renderer, 10, 0, 20, 255);
 	SDL_RenderClear(renderer);
@@ -271,17 +274,31 @@ void doRender(SDL_Renderer* renderer, Man* man, Bullet* mermi, Enemy* enemyler) 
 	SDL_Rect man_range_rect = { man->x + man->size / 2 - man->range ,man->y + man->size / 2 - man->range, man->range * 2, man->range * 2 };
 	SDL_RenderDrawRect(renderer, &man_range_rect);
 
+	for (int n = 0; n < 30; n++) {
+		if (mermiler[n].life != 0) {
 
-	if (mermi->life != 0) {
+			SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255);
+			SDL_Rect bullet_1 = { mermiler[n].x,mermiler[n].y,mermiler[n].width,mermiler[n].height };
+			SDL_RenderFillRect(renderer, &bullet_1);
 
-
-		SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255);
-		SDL_Rect bullet_1 = { mermi->x,mermi->y,mermi->width,mermi->height };
-		SDL_RenderFillRect(renderer, &bullet_1);
-
-
+		}
 	}
 
+	SDL_Rect mermiler_1[30];
+	for (int n = 0; n < 30; n++) {
+		mermiler_1[n].x = mermiler[n].x;
+		mermiler_1[n].y = mermiler[n].y;
+		mermiler_1[n].w = mermiler[n].width;
+		mermiler_1[n].h = mermiler[n].height;
+	}
+
+	for (int n = 0; n < 30; n++) { //her mermiyi ayrı ayrı print ediyorum
+
+		if (mermiler[n].life != 0) {
+			SDL_SetRenderDrawColor(renderer, 204, 20, 20, 255);
+			SDL_RenderFillRect(renderer, &mermiler[n]);
+		}
+	}
 
 
 	SDL_Rect enemy_1[enemy_count];
@@ -351,35 +368,50 @@ int main(int argc, char* argv[]) {
 	TTF_Init();
 
 	Man man;
-	man.x = 220;
-	man.y = 310;
+	man.x = 720;
+	man.y = 410;
 	man.size = 50;
 	man.puan = 0;
 	man.range = 230;
 
-	Bullet mermi;
-	mermi.x = 10000;
-	mermi.y = 10000;
-	mermi.width = 20;
-	mermi.height = 20;
-	mermi.speedx = 10;
-	mermi.speedy = 10;
+	Bullet mermiler[30];
+	for (int n = 0; n < 30; n++) {
+		mermiler[n].y = 10000;
+		mermiler[n].x = 10000;
+		mermiler[n].width = 15;
+		mermiler[n].height = 15;
+		mermiler[n].speedx = 10;
+		mermiler[n].speedy = 10;
+		mermiler[n].biterken = 10;
+		mermiler[n].life = 0;
+	}
 
-
+	int enemy_start_coord[60][2] = { // enemylerin konumlarını manuel ayarlayacağım ondan for kullanmadım henüz.
+		{10,10},{WINDOW_WIDTH - 100,10},{10,WINDOW_HEIGHT - 100},{WINDOW_WIDTH - 100,WINDOW_HEIGHT - 100},{WINDOW_WIDTH/2,10},{WINDOW_WIDTH / 2,WINDOW_HEIGHT -100},
+		{10,10},{WINDOW_WIDTH - 100,10},{10,WINDOW_HEIGHT - 100},{WINDOW_WIDTH - 100,WINDOW_HEIGHT - 100},{WINDOW_WIDTH / 2,10},{WINDOW_WIDTH / 2,WINDOW_HEIGHT - 100},
+		{10,10},{WINDOW_WIDTH - 100,10},{10,WINDOW_HEIGHT - 100},{WINDOW_WIDTH - 100,WINDOW_HEIGHT - 100},{WINDOW_WIDTH / 2,10},{WINDOW_WIDTH / 2,WINDOW_HEIGHT - 100},
+		{10,10},{WINDOW_WIDTH - 100,10},{10,WINDOW_HEIGHT - 100},{WINDOW_WIDTH - 100,WINDOW_HEIGHT - 100},{WINDOW_WIDTH / 2,10},{WINDOW_WIDTH / 2,WINDOW_HEIGHT - 100},
+		{10,10},{WINDOW_WIDTH - 100,10},{10,WINDOW_HEIGHT - 100},{WINDOW_WIDTH - 100,WINDOW_HEIGHT - 100},{WINDOW_WIDTH / 2,10},{WINDOW_WIDTH / 2,WINDOW_HEIGHT - 100},
+		{10,10},{WINDOW_WIDTH - 100,10},{10,WINDOW_HEIGHT - 100},{WINDOW_WIDTH - 100,WINDOW_HEIGHT - 100},{WINDOW_WIDTH / 2,10},{WINDOW_WIDTH / 2,WINDOW_HEIGHT - 100},
+		{10,10},{WINDOW_WIDTH - 100,10},{10,WINDOW_HEIGHT - 100},{WINDOW_WIDTH - 100,WINDOW_HEIGHT - 100},{WINDOW_WIDTH / 2,10},{WINDOW_WIDTH / 2,WINDOW_HEIGHT - 100},
+		{10,10},{WINDOW_WIDTH - 100,10},{10,WINDOW_HEIGHT - 100},{WINDOW_WIDTH - 100,WINDOW_HEIGHT - 100},{WINDOW_WIDTH / 2,10},{WINDOW_WIDTH / 2,WINDOW_HEIGHT - 100},
+		{10,10},{WINDOW_WIDTH - 100,10},{10,WINDOW_HEIGHT - 100},{WINDOW_WIDTH - 100,WINDOW_HEIGHT - 100},{WINDOW_WIDTH / 2,10},{WINDOW_WIDTH / 2,WINDOW_HEIGHT - 100},
+		{10,10},{WINDOW_WIDTH - 100,10},{10,WINDOW_HEIGHT - 100},{WINDOW_WIDTH - 100,WINDOW_HEIGHT - 100},{WINDOW_WIDTH / 2,10},{WINDOW_WIDTH / 2,WINDOW_HEIGHT - 100},
+	};
 	Enemy enemyler[enemy_count];
 	for (int n = 0; n < enemy_count; n++) {
-		enemyler[n].x = 400 + (n * 100);
-		enemyler[n].y = 400;
-		enemyler[n].width = 40 + n * 10;
-		enemyler[n].height = 40 + n * 10;
-		enemyler[n].life = 1 + (n * 1);
-		enemyler[n].speed = n * 10;
+		enemyler[n].y = enemy_start_coord[n][1];
+		enemyler[n].x = enemy_start_coord[n][0];
+		enemyler[n].width = 200 - n * 20;
+		enemyler[n].height = 200 - n * 20;
+		enemyler[n].life = 1 + (n * 3);
+		enemyler[n].speed = 50 + 5*n;
 	}
 
 
 
 
-	window = SDL_CreateWindow("Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1424, 740, SDL_WINDOW_BORDERLESS);
+	window = SDL_CreateWindow("Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_BORDERLESS);
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
 
 	//SDL_SetWindowFullscreen(window,SDL_WINDOW_FULLSCREEN);
@@ -390,9 +422,9 @@ int main(int argc, char* argv[]) {
 	int done = 0;
 	Mix_PlayChannel(1, background, -1);
 	while (!done) {
-		done = processEvents(window, &man, &mermi, &enemyler,hitEffect,shootEffect);
+		done = processEvents(window, &man, &mermiler, &enemyler,hitEffect,shootEffect);
 		//render display
-		doRender(renderer, &man, &mermi, &enemyler);
+		doRender(renderer, &man, &mermiler, &enemyler);
 	}
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
