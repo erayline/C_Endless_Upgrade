9ddf54e38134ebdd20be7c5d683042c832c0dab9
erayline <144234072+erayline@users.noreply.github.com>
2023-11-29 14:39:41 +0300

    Update main.c
    range ekledim.  en yakındaki adamı da buluyor. eğer range içinde düşman yoksa ateş etmiyor

--- main.c
+++ main.c
@@ -3,31 +3,92 @@
 #include "mystructs.h"
 #include <SDL_ttf.h>
 #include <SDL_mixer.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <math.h>
 
 #define FPS = 30
 
 #define FRAME_TARGET_TIME = (1000/FPS) //her frame kaç ms sürecek
-#define enemy_count 3
+#define enemy_count 4
 
+int calculate_distance(int deger1,int deger2) { // iki sayının farkının mutlağını bulan fonksiyon tanımladım uzaklık bulurken falan kullanıcam.
+	int sonuc =0;
+	return abs(deger1 - deger2);
+}
 
-int atack_speed = 10;
+int yakinlik[enemy_count]; // bu yakınlık listesi. en sondaki eleman en yakındaki eleman oluyor.
+
+int ilk_buyukmu(int deger1,int deger2) { 
+	if (deger1 >= deger2) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+int atack_speed = 400;
 int bullet_speed = 1000;
-
 int last_frame_time = 1;
 int biterken = 0;
+int bullet_x_speed = 0;
+int bullet_y_speed = 0; // burada birsürü gereksiz şey tanımlamışım bir ara azaltıcam.
+int processEvents(SDL_Window *window, Man *man,Bullet *mermi, Enemy *enemyler) { // burada işlemler oluyor
 
-int processEvents(SDL_Window *window, Man *man,Bullet *mermi, Enemy *enemy, Enemy *enemyler) {
+	int man_ortasiX = man->x + man->size / 2;
+	int man_ortasiY = man->y + man->size / 2;  // sürekli adamın ortasını bulmaya çalıştığım için kısalttım bu şekilde
+
+	// burada bubble sort inşa ettim sıfırdan kendim. işime yarayacak kadar çalışıyor.
+	int uzaklikSirasi[enemy_count][2];
+	int tutucuX,tutucuY,tutucuP;//pisagor
+	for (int n = 0; n < enemy_count; n++) {    // bubble sort için uzaklıkları listede toplama
+		tutucuX = calculate_distance(man_ortasiX, enemyler[n].x + enemyler[n].width/2);
+		tutucuY = calculate_distance(man_ortasiY, enemyler[n].y + enemyler[n].height / 2);
+
+		tutucuP = sqrt(tutucuX*tutucuX + tutucuY*tutucuY);
+		uzaklikSirasi[n][0] = tutucuP;
+		uzaklikSirasi[n][1] = n;
+	}
+	
+	int tutucu2;
+	int tutucu2_n;
+	for (int n = 0; n < enemy_count - 1; n++) { // bubblesortun kendisi
+
+		if (!(ilk_buyukmu(uzaklikSirasi[n][0], uzaklikSirasi[n + 1][0]))) {
+			tutucu2 = uzaklikSirasi[n][0];
+			tutucu2_n = uzaklikSirasi[n][1];
+
+			uzaklikSirasi[n][0] = uzaklikSirasi[n + 1][0];
+			uzaklikSirasi[n][1] = uzaklikSirasi[n + 1][1];
+
+			uzaklikSirasi[n + 1][0] = tutucu2;
+			uzaklikSirasi[n + 1][1] = tutucu2_n;
+		}
+	}
+	int closest_enemy_index = uzaklikSirasi[enemy_count - 1][1]; // en yakın enemy
+	
+
+
+
+
+
+
+
 	SDL_Event event;
 	int done = 0;
-	float delta_time = (SDL_GetTicks() - last_frame_time) / 1000.0f;
+	float delta_time = (SDL_GetTicks() - last_frame_time) / 1000.0f; // burada fps ile ilgili ayarlar var
 	last_frame_time = SDL_GetTicks();
 
-	Mix_Chunk* shootEffect = Mix_LoadWAV("laserShoot.wav");
-	Mix_Chunk* hitEffect = Mix_LoadWAV("hitHurt.wav");
+	Mix_Chunk* shootEffect = Mix_LoadWAV("laserShoot.wav"); // ses yükledim
+	Mix_Chunk* hitEffect = Mix_LoadWAV("hitHurt.wav"); // ses yükledim
+
+
+
 
 
 	// moving part
-	while (SDL_PollEvent(&event)) {
+	while (SDL_PollEvent(&event)) {  //klavye harektlerini detect
 
 		SDL_Delay(20);
 
@@ -43,9 +104,9 @@ int processEvents(SDL_Window *window, Man *man,Bullet *mermi, Enemy *enemy, Enem
 				case (SDLK_ESCAPE):
 					done = 1;
 					break;
-				case SDLK_SPACE:
+				case SDLK_SPACE: //denemek için
 					bullet_speed += 10;
-					//atack_speed -= 10;
+					atack_speed -= 10;
 					break;
 			}
 			break;
@@ -57,20 +118,38 @@ int processEvents(SDL_Window *window, Man *man,Bullet *mermi, Enemy *enemy, Enem
 	}
 
 	const Uint8* state = SDL_GetKeyboardState(NULL);
+	
 	//mermi fonksiyonları
 
-	if (mermi->life != 1) {
-		if (atack_speed < (SDL_GetTicks() - biterken)) { //burada iki saniyede bir ateş etsin diye ayarlamaya çalıştım ama olmadı nedense.
-			mermi->x = man->x + (man->size) / 2 - (mermi->width) / 2;
-			mermi->y = man->y - man->size / 10;
-			mermi->life = 1;
-			Mix_PlayChannel(-1, shootEffect, 0);
+
+	
+
+
+	for (int n = 0; n < enemy_count; n++) { //burada range içine giren eleman olursa ateş etsin diye ayarladım
+		
+		if (((((enemyler[n].x > man->x - man->range) && (enemyler[n].x < man->x + man->range)) && ((enemyler[n].y > man->y - man->range) && (enemyler[n].y < man->y + man->range))) || (((enemyler[n].x + enemyler[n].width > man->x - man->range) && (enemyler[n].x + enemyler[n].width < man->x + man->range)) && ((enemyler[n].y + enemyler[n].height > man->y - man->range) && (enemyler[n].y + enemyler[n].height < man->y + man->range))))) {
+			
+			if (mermi->life != 1) { // halihazırda ateşlenmemiş mermiyi ateşliyor
+				if (atack_speed < (SDL_GetTicks() - biterken)) { //burada iki saniyede bir ateş etsin diye ayarlamaya çalıştım 
+					mermi->x = man->x + (man->size) / 2 - (mermi->width) / 2;
+					mermi->y = man->y - man->size / 10;
+					mermi->life = 1;
+					Mix_PlayChannel(-1, shootEffect, 0);
+
+				}
+				else {
+					//printf("mermi zaamanı gelmedi");
+				}
+			}
 
 		}
-		else {
-			//printf("mermi zaamanı gelmedi");
-		}
+
+
 	}
+
+
+
+	
 
 	if (mermi->life == 1) {
 		mermi->y -= bullet_speed * delta_time;
@@ -108,7 +187,7 @@ int processEvents(SDL_Window *window, Man *man,Bullet *mermi, Enemy *enemy, Enem
 			enemyler[n].x = -1000;
 		}
 		else {
-			if (enemyler[n].life != 0) {
+			if (enemyler[n].life != 0) { // enemylerin hızlarını ayarladığım bölüm
 				if (enemyler[n].y + enemyler[n].height/2 > man->y + man->size/2) {
 					enemyler[n].y -= enemyler[n].speed * delta_time;
 				}
@@ -124,9 +203,11 @@ int processEvents(SDL_Window *window, Man *man,Bullet *mermi, Enemy *enemy, Enem
 				}
 			}
 
+
+
 			if ((((enemyler[n].x < mermi->x) && (enemyler[n].x + enemyler[n].width > mermi->x)) || ((enemyler[n].x < mermi->x + mermi->width) && (enemyler[n].x + enemyler[n].width > mermi->x + mermi->width))) && (((enemyler[n].y < mermi->y) && (enemyler[n].y + enemyler[n].height > mermi->y)) || ((enemyler[n].y < mermi->y + mermi->height) && (enemyler[n].y + enemyler[n].height > mermi->y + mermi->height)))) {
 				enemyler[n].life -= 1;
-				man->puan += 1;
+				man->puan += 1; // mermi için collision detection
 				mermi->life = 0;
 				Mix_PlayChannel(-1, hitEffect, 0);
 				biterken = SDL_GetTicks();
@@ -135,17 +216,15 @@ int processEvents(SDL_Window *window, Man *man,Bullet *mermi, Enemy *enemy, Enem
 		}
 
 
+
 	}
-
-
-
 
 	return done;
 }
 
 
 
-void doRender(SDL_Renderer* renderer, Man* man, Bullet *mermi, Enemy *enemy, Enemy *enemyler) {
+void doRender(SDL_Renderer* renderer, Man* man, Bullet *mermi, Enemy *enemyler) {
 
 	SDL_SetRenderDrawColor(renderer, 10, 0, 20, 255);
 	SDL_RenderClear(renderer);
@@ -154,16 +233,21 @@ void doRender(SDL_Renderer* renderer, Man* man, Bullet *mermi, Enemy *enemy, Ene
 	SDL_SetRenderDrawColor(renderer, 20, 204, 84, 255);
 	SDL_Rect rect = { man -> x,man -> y, man->size,man->size };
 	SDL_RenderFillRect(renderer, &rect); // burada içini dolduracağımız cismin adresine atıfta bulunuyoruz.
+	
+	SDL_Rect man_range_rect = { man->x + man->size/2 - man->range ,man->y +man->size/2 - man->range, man->range *2, man->range*2 };
+	SDL_RenderDrawRect(renderer,&man_range_rect,1);
 
 	if (mermi->life != 0) {
 		
-
+		
 		SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255);
 		SDL_Rect bullet_1 = { mermi -> x,mermi -> y,mermi -> width,mermi -> height };
 		SDL_RenderFillRect(renderer, &bullet_1);
 
 
 	}
+
+	
 
 	SDL_Rect enemy_1[enemy_count];
 	SDL_Rect enemy_1_dec[enemy_count];
@@ -179,7 +263,7 @@ void doRender(SDL_Renderer* renderer, Man* man, Bullet *mermi, Enemy *enemy, Ene
 		enemy_1_dec[n].h = enemyler[n].height - enemyler[n].height / 2;
 	}
 
-	for (int n = 0; n < enemy_count; n++) {
+	for (int n = 0; n < enemy_count; n++) { //her düşmanı ayrı ayrı print ediyorum
 
 		if (enemyler[n].life != 0) {
 			SDL_SetRenderDrawColor(renderer, 204, 20, 20, 255);
@@ -199,46 +283,36 @@ int main(int argc, char* argv[]) {
 	SDL_Window* window;  //window tanımlama
 	SDL_Renderer* renderer; //renderer tanımlama
 	SDL_Init(SDL_INIT_EVERYTHING); // sdl'i çalıştırıyor 
+	Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048);
 
 	//TTF_Init();
 	//TTF_Font* font = TTF_OpenFont("arial.ttf", 24);
+	//Mix_Music* deneme = Mix_LoadWav("laserShoot.wav");
 
 	Man man;
 	man.x = 220;
 	man.y = 310;
 	man.size = 50;
 	man.puan = 0;
-
-	Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048);
-
-
-	//Mix_Music* deneme = Mix_LoadWav("laserShoot.wav");
-
+	man.range = 200;
 
 	Bullet mermi;
 	mermi.x = 10000; 
 	mermi.y = 10000;
 	mermi.width = 20;
 	mermi.height = 20;
-	
-	Enemy enemy;
-	enemy.x = 400;
-	enemy.y = 30;
-	enemy.width = 60;
-	enemy.height = 60;
-	enemy.life = 1;
 
 	Mix_Chunk* bornEffect = Mix_LoadWAV("synth.wav");
 
 
 	Enemy enemyler[enemy_count];
 	for (int n = 0; n < enemy_count; n++) {
-		enemyler[n].x =  300 + n * 300;
+		enemyler[n].x =  400 + (n * 100);
 		enemyler[n].y = 400;
 		enemyler[n].width = 40 + n*10;
 		enemyler[n].height =  40 + n*10;
-		enemyler[n].life = n*10;
-		enemyler[n].speed =  140 + 10*n ;
+		enemyler[n].life = 1 + (n*10);
+		enemyler[n].speed =  0 ;
 	}
 
 	
@@ -247,17 +321,19 @@ int main(int argc, char* argv[]) {
 	window = SDL_CreateWindow("Game",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,1424,740,SDL_WINDOW_BORDERLESS);
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
 
-	SDL_SetWindowFullscreen(window,SDL_WINDOW_FULLSCREEN);
+	//SDL_SetWindowFullscreen(window,SDL_WINDOW_FULLSCREEN);
 
 
+	Mix_Music* background = Mix_LoadWAV("backgro.wav");
 
 	int done = 0;
-		Mix_PlayChannel(-1, bornEffect, 0);
+	Mix_PlayChannel(1, background, -1);
+	Mix_PlayChannel(-1, bornEffect, 0);
 	while (!done) {
 
-		done = processEvents(window, &man, &mermi, &enemy, &enemyler);
+		done = processEvents(window, &man, &mermi,&enemyler);
 		//render display
-		doRender(renderer, &man, &mermi, &enemy,&enemyler);
+		doRender(renderer, &man, &mermi, &enemyler);
 	}
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
