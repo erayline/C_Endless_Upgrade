bca9a00f484dff4e5ce4213369f993428ac601f7
Eray <144234072+erayline@users.noreply.github.com>
2023-12-13 00:06:13 +0300

    Update mystructs.h

--- mystructs.h
+++ mystructs.h
@@ -2,17 +2,30 @@
 #define STRUCTER_H
 
 #define FPS = 1
-#define WINDOW_WIDTH 1220 // ön şişmanlı komutları
-#define WINDOW_HEIGHT 880
+#define WINDOW_WIDTH 1920 // ön şişmanlı komutları
+#define WINDOW_HEIGHT 1080
 #define FRAME_TARGET_TIME = (1000/FPS) //her frame kaç ms sürecek
 #define enemy_count 100
 #define mermi_count 40
-#define MAP_WIDTH 3000
-#define MAP_HEIGHT 3000
+#define MAP_WIDTH 12000
+#define MAP_HEIGHT 8000
+
+#define enemy_life_bar_width 80
+#define enemy_life_bar_height 20
+#define life_bar_width 380
+#define life_bar_height 40
 
 int enemy1_walk_spreadsheet_width = 0;
 
 int last_frame_time_renderer;
+SDL_Rect camera = { 0,0,WINDOW_WIDTH,WINDOW_HEIGHT };
+
+int upgrade_choice = 0;
+float speed_constant;
+int last_bullet_shot = 10;
+int yakinlik[enemy_count]; // bu yakınlık listesi. en sondaki eleman en yakındaki eleman oluyor.
+int last_collision_man = 0;
+
 
 
 int map_x = 0;
@@ -20,9 +33,12 @@ int map_y = 0;
 
 
 typedef struct {
-	int time_counter;
+
+	int time_counter_min;
+	int time_counter_sec;
 	int difficulty_factor;
-	int level;
+	int state;
+
 }Game_stats;
 
 typedef struct {
@@ -30,18 +46,19 @@ typedef struct {
 }GUI_State;
 
 typedef struct {
+	char name[20];
 	int x, y;
-	int current_life;
-	int max_life;
 	int size;
 	int range;
 	int puan;
 	int money;
+
+	int current_life;
+	int max_life;
 	int bullet_power;
 	int bullet_speed;
-	char name[20];
-	int move_speed;
 	int attack_speed;
+	int move_speed;
 
 	int idle_image_timer;
 	int idle_image_width_increment;
@@ -76,7 +93,11 @@ typedef struct {
 	float x;
 	float y;
 	int color;
-	int life;
+
+	int max_life;
+	int current_life;
+	int life_bar_active;
+
 	int width;
 	int height;
 	int speed;
@@ -99,5 +120,26 @@ typedef struct {
 	SDL_Texture* map_image_texture;
 	SDL_Texture* enemy1_image_texture;
 }Game_assets;
+
+
+typedef struct {
+	int x;
+	int y;
+	int width;
+	int height;
+	int amount;
+	int life;
+	int item_index;
+	SDL_Texture* item_texture;
+}Item;
+
+typedef struct {
+	int x;
+	int y;
+	int width;
+	int height;
+	int price;
+	int isActive;
+}Chest;
 
 #endif
